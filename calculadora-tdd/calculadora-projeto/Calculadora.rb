906d6e4b8a55ff31eb079e880c5950f61facbcd3
Rhodrigo <rhodrigolpicinini@hotmail.com>
2022-07-28 17:44:39 -0300

    Informando operador incorreto em 'calcular' e comentários adicionados

--- calculadora-tdd/calculadora-projeto/Calculadora.rb
+++ calculadora-tdd/calculadora-projeto/Calculadora.rb
@@ -1,7 +1,9 @@
 class Calculadora
 
+    #Lista de valores inválidos
     @@invalid = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","z","ç"]
 
+    #Operações
     def somar(v1, v2)
         resultado = v1 + v2
         resultado
@@ -32,6 +34,7 @@ class Calculadora
         resultado
     end
 
+    #Método calcular, recebe dois valores e o operador e retorna o valor da expressão formada
     def calcular(v1, v2, op)
         begin
             case op
@@ -47,15 +50,19 @@ class Calculadora
                 return "#{v1} ** #{v2} = #{potencia(v1, v2)}"
             when "%"
                 return "#{v1} % #{v2} = #{modulo(v1, v2)}"
+            else
+                return "Operador desconhecido!"
             end
         rescue TypeError
             "Valor inserido não é Integer ou Float"
         end
     end
 
+    #Método calcularEval, recebe uma expressão completa em forma de string e resolve ela a partir do método 'eval'
     def calcularEval(expression)
         begin
             expressionArray = expression.split("")
+            #Verifica se a expressão contém algum valor inválido, estabelecido pelo array '@@invalid'
             expressionArray.each do |valor|
                 if @@invalid[0..@@invalid.length].include?(valor)
                     return "Não é possível processar letras na expressão!"
