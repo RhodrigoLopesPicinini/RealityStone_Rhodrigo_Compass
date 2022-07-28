31180fe9bb49909b05f85269f57dfc398bb6af5c
Rhodrigo <rhodrigolpicinini@hotmail.com>
2022-07-28 16:21:43 -0300

    Lidando com valores de tipo String

--- calculadora-tdd/calculadora-projeto/Program.rb
+++ calculadora-tdd/calculadora-projeto/Program.rb
@@ -1,24 +1,30 @@
 require_relative 'Calculadora'
 
 def calcular(v1, v2, op)
-    c = Calculadora.new
-    case op
-    when "+"
-        return "#{v1} + #{v2} = #{c.somar(v1, v2)}"
-    when "-"
-        return "#{v1} - #{v2} = #{c.subtrair(v1, v2)}"
-    when "*"
-        return "#{v1} * #{v2} = #{c.multiplicar(v1, v2)}"
-    when "/"
-        return "#{v1} / #{v2} = #{c.dividir(v1, v2)}"
-    when "**"
-        return "#{v1} ** #{v2} = #{c.potencia(v1, v2)}"
-    when "%"
-        return "#{v1} % #{v2} = #{c.modulo(v1, v2)}"
+    if v1.class or v2.class == String
+        return "Valor inserido não é inteiro ou flutuante"
+    
+    else
+    
+        c = Calculadora.new
+        case op
+        when "+"
+            return "#{v1} + #{v2} = #{c.somar(v1, v2)}"
+        when "-"
+            return "#{v1} - #{v2} = #{c.subtrair(v1, v2)}"
+        when "*"
+            return "#{v1} * #{v2} = #{c.multiplicar(v1, v2)}"
+        when "/"
+            return "#{v1} / #{v2} = #{c.dividir(v1, v2)}"
+        when "**"
+            return "#{v1} ** #{v2} = #{c.potencia(v1, v2)}"
+        when "%"
+            return "#{v1} % #{v2} = #{c.modulo(v1, v2)}"
+        end
     end
         
 end
 
-puts calcular(2, 2, "**")
+puts calcular(2.5, "2.0", "**")
 
 
