24df65752c745666c6aa5ac6055008dd33e5bb1c
Rhodrigo <rhodrigolpicinini@hotmail.com>
2022-07-28 16:38:30 -0300

    Métodos 'calcularEval' e 'calcular' implementados

--- calculadora-tdd/calculadora-projeto/Program.rb
+++ calculadora-tdd/calculadora-projeto/Program.rb
@@ -1,30 +1,11 @@
 require_relative 'Calculadora'
 
-def calcular(v1, v2, op)
-    if v1.class or v2.class == String
-        return "Valor inserido não é inteiro ou flutuante"
-    
-    else
-    
-        c = Calculadora.new
-        case op
-        when "+"
-            return "#{v1} + #{v2} = #{c.somar(v1, v2)}"
-        when "-"
-            return "#{v1} - #{v2} = #{c.subtrair(v1, v2)}"
-        when "*"
-            return "#{v1} * #{v2} = #{c.multiplicar(v1, v2)}"
-        when "/"
-            return "#{v1} / #{v2} = #{c.dividir(v1, v2)}"
-        when "**"
-            return "#{v1} ** #{v2} = #{c.potencia(v1, v2)}"
-        when "%"
-            return "#{v1} % #{v2} = #{c.modulo(v1, v2)}"
-        end
-    end
-        
-end
+c = Calculadora.new
 
-puts calcular(2.5, "2.0", "**")
+puts c.calcularEval("1+1")
+#Resultado: 1+1 = 2
+
+puts c.calcular(2, 2, "**")
+#Resultado: 2 ** 2 = 4
 
 
