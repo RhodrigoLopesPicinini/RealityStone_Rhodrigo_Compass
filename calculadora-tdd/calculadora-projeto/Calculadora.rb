b71c30b96ccdf067f3f46c3fcc7c94726408aefe
Rhodrigo <rhodrigolpicinini@hotmail.com>
2022-07-28 16:36:00 -0300

    Método calcular movido para classe Calculadora

--- calculadora-tdd/calculadora-projeto/Calculadora.rb
+++ calculadora-tdd/calculadora-projeto/Calculadora.rb
@@ -30,4 +30,31 @@ class Calculadora
         resultado
     end
 
+    def calcular(v1, v2, op)
+        if(v1.class == String or v2.class == String)
+            return "Valor inserido não é inteiro ou flutuante"
+        
+        else
+            case op
+            when "+"
+                return "#{v1} + #{v2} = #{somar(v1, v2)}"
+            when "-"
+                return "#{v1} - #{v2} = #{subtrair(v1, v2)}"
+            when "*"
+                return "#{v1} * #{v2} = #{multiplicar(v1, v2)}"
+            when "/"
+                return "#{v1} / #{v2} = #{dividir(v1, v2)}"
+            when "**"
+                return "#{v1} ** #{v2} = #{potencia(v1, v2)}"
+            when "%"
+                return "#{v1} % #{v2} = #{modulo(v1, v2)}"
+            end
+        end
+            
+    end
+
+    def calcularEval(expression)
+        return "#{expression} = #{eval(expression)}"
+    end
+
 end
