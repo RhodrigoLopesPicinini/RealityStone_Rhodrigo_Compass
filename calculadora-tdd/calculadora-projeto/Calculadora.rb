8590d1b71a617c0007377a74aad59860e5c0ec95
Rhodrigo <rhodrigolpicinini@hotmail.com>
2022-07-28 16:06:00 -0300

    Criação da classe Calculadora e operações básicas

---
+++ calculadora-tdd/calculadora-projeto/Calculadora.rb
@@ -0,0 +1,23 @@
+class Calculadora
+
+    def somar(v1, v2)
+        resultado = v1 + v2
+        resultado
+    end
+
+    def subtrair(v1, v2)
+        resultado = v1 - v2
+        resultado
+    end
+
+    def dividir(v1, v2)
+        resultado = v1 / v2
+        resultado
+    end
+
+    def multiplicar(v1, v2)
+        resultado = v1 * v2
+        resultado
+    end
+
+end
