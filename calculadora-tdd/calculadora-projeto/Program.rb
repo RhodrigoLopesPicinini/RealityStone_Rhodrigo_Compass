5a57657a527f356a2ec4ed2162ed780b99090385
Rhodrigo <rhodrigolpicinini@hotmail.com>
2022-07-29 09:22:58 -0300

    Menu de interface para uso de usuário

--- calculadora-tdd/calculadora-projeto/Program.rb
+++ calculadora-tdd/calculadora-projeto/Program.rb
@@ -2,10 +2,73 @@ require_relative 'Calculadora'
 
 c = Calculadora.new
 
-puts c.calcularEval("1+1")
-#Resultado: 1+1 = 2
+def definirNumeros
+    print "Primeiro valor: "
+    v1 = gets.chomp.to_i
+    print "Segundo valor: "
+    v2 = gets.chomp.to_i
+    if v1 != Integer || v1 != Float || v2 != Integer || v2 != Float
+        raise NameError
+    else
+        return v1, v2
+    end
+end
 
-puts c.calcular(2, 2, "**")
-#Resultado: 2 ** 2 = 4
+loop do
+
+    begin
+        puts "=====================CALCULADORA======================="
+        puts "Deseja calcular com qual operador?"
+        puts "+    : Soma                     | /  : Divisão      |"
+        puts "-    : Subtração                | %  : Módulo       |"
+        puts "*    : Multiplicação            | ** : Potênciação  |"
+        puts "eval : Insira uma expressão     |"
+        puts "x    : Sair                     |"
+        puts "======================================================="
+        operador = gets.chomp.downcase
+        case operador
+        when "+"
+            v1, v2 = definirNumeros
+            puts c.calcular(v1, v2, "+")
+        when "-"
+            v1, v2 = definirNumeros
+            puts c.calcular(v1, v2, "-")
+        when "*"
+            v1, v2 = definirNumeros
+            puts c.calcular(v1, v2, "*")
+        when "/"
+            v1, v2 = definirNumeros
+            puts c.calcular(v1, v2, "/")
+        when "%"
+            v1, v2 = definirNumeros
+            puts c.calcular(v1, v2, "%")
+        when "**"
+            v1, v2 = definirNumeros
+            puts c.calcular(v1, v2, "**")
+        when "eval"
+            puts "Escreva a expressão que deseja calcular:"
+            expr = gets.chomp.to_s
+            puts c.calcularEval(expr)
+        when "x"
+            puts "Saindo..."
+            break
+        else
+            puts "Operador não existe!"
+        end
+
+        print "Enter para continuar"
+        gets
+
+    rescue NameError
+        p "Insira valores válidos para a operação"
+        print "Retornando a calculadora, enter para continuar"
+        input = gets
+    end
+
+    system("clear") || system("cls")
+end
+
+
+
 
 
