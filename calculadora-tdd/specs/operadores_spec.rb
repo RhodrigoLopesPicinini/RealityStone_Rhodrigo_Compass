d2b0330d3ed22f1403f1af6c50a8d3d211974821
Rhodrigo <rhodrigolpicinini@hotmail.com>
2022-07-28 21:12:08 -0300

    Adição de testes com dois valores negativos

--- calculadora-tdd/specs/operadores_spec.rb
+++ calculadora-tdd/specs/operadores_spec.rb
@@ -10,8 +10,12 @@ describe('Calculadora') do
         expect(calculadora.somar(1, 4)).to eq(5)
     end
 
-    it("Soma os valores com um valor negativo -1 e 4") do
-        expect(calculadora.somar(-1, 4)).to eq(3)
+    it("Soma os valores com um valor negativo -1 e 5") do
+        expect(calculadora.somar(-1, 5)).to eq(4)
+    end
+
+    it("Soma os valores negativos -3 e -4") do
+        expect(calculadora.somar(-3, -4)).to eq(-7)
     end
 
     it("Soma os valores flutuantes 1.5 e 4.2") do
@@ -23,12 +27,16 @@ describe('Calculadora') do
         expect(calculadora.subtrair(6, 2)).to eq(4)
     end
 
-    it("Subtração dos valores com um negativo 6 e -2 ") do
-        expect(calculadora.subtrair(6, -2)).to eq(8)
+    it("Subtração dos valores com um negativo 6 e -6 ") do
+        expect(calculadora.subtrair(6, -6)).to eq(12)
+    end
+
+    it("Subtração dos valores negativos -6 e -3 ") do
+        expect(calculadora.subtrair(-6, -3)).to eq(-3)
     end
 
     it("Subtração dos valores flutuantes 6.2 e 2.2 ") do
-        expect(calculadora.subtrair(6.2, 2.1)).to eq(4.1)
+        expect(calculadora.subtrair(6.2, 2.2)).to eq(4.0)
     end
 
     #TESTES DE MULTIPLICAÇÃO
@@ -36,12 +44,16 @@ describe('Calculadora') do
         expect(calculadora.multiplicar(2, 5)).to eq(10)
     end
 
-    it("Multiplicação dos valores com um negativo -2 e 5") do
-        expect(calculadora.multiplicar(-2, 5)).to eq(-10)
+    it("Multiplicação dos valores com um negativo -2 e 10") do
+        expect(calculadora.multiplicar(-2, 10)).to eq(-20)
     end
 
-    it("Multiplicação dos valores flutuantes 2.6 e 5.2") do
-        expect(calculadora.multiplicar(2.6, 5.2)).to eq(13.52)
+    it("Multiplicação dos valores negativos -3 e -12") do
+        expect(calculadora.multiplicar(-3, -12)).to eq(36)
+    end
+
+    it("Multiplicação dos valores flutuantes 3.6 e 7.2") do
+        expect(calculadora.multiplicar(3.6, 7.2)).to eq(25.92)
     end
 
     #TESTES DE DIVISÃO
@@ -53,8 +65,12 @@ describe('Calculadora') do
         expect(calculadora.dividir(-9, 3)).to eq(-3)
     end
 
-    it("Divisão dos valores flutuantes 9.0 e 3") do
-        expect(calculadora.dividir(9.0, 3)).to eq(3.0)
+    it("Divisão dos valores negativos -4 e -2") do
+        expect(calculadora.dividir(-4, -2)).to eq(2)
+    end
+
+    it("Divisão dos valores flutuantes 9.0 e 3.2") do
+        expect(calculadora.dividir(9.0, 3.2)).to eq(2.81)
     end
 
     #TESTES DE POTENCIAÇÃO
@@ -62,12 +78,12 @@ describe('Calculadora') do
         expect(calculadora.potencia(9, 2)).to eq(81)
     end
 
-    it("Potenciação dos valores com um negativo -9 elevado a 2") do
-        expect(calculadora.potencia(-9, 2)).to eq(81)
+    it("Potenciação dos valores com um negativo -10 elevado a 3") do
+        expect(calculadora.potencia(-10, 3)).to eq(-1000)
     end
 
-    it("Potenciação dos valores flutuantes 9 elevado a 2") do
-        expect(calculadora.potencia(9.2, 2)).to eq(84.64)
+    it("Potenciação dos valores flutuantes 4.3 elevado a 5.5") do
+        expect(calculadora.potencia(4.3, 5.5)).to eq(3048.43)
     end
 
     #TESTES DE MÓDULO/RESTO
@@ -77,6 +93,10 @@ describe('Calculadora') do
 
     it("Módulo/Resto dos valores com um negativo -2 e 4") do
         expect(calculadora.modulo(-2, 4)).to eq(2)
+    end
+
+    it("Módulo/Resto dos valores negativos -3 e -4") do
+        expect(calculadora.modulo(-3, -4)).to eq(-3)
     end
 
     it("Módulo/Resto dos valores flutuantes 2.2 e 2") do
